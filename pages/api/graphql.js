5ae52101c43673c19e282b4fd299b1b74c2f24ba
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-07-30 22:29:09 +0530

    WIP: notifications

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,17 +1,38 @@
-import { createServer } from '@graphql-yoga/node';
+import 'event-target-polyfill';
+import { createServer, createPubSub } from '@graphql-yoga/node';
+
+const pubsub = createPubSub();
 
 const server = createServer({
   schema: {
-    typeDefs: /* GraphQL */ `
+    typeDefs: `
       type Query {
         hello: String
+      }
+      type Mutation {
+        hello: String
+      }
+      type Subscription {
+        notification: String
       }
     `,
     resolvers: {
       Query: {
         hello: () => 'Hello from Yoga!',
       },
-    },
+      Mutation: {
+        hello: () => {
+          pubsub.publish('notification', `Hello GraphQL Subscription.Time: ${new Date()}`);
+          return 'Hello from Yoga!';
+        },
+      },
+      Subscription: {
+        notification: {
+          subscribe: () => pubsub.subscribe('notification'),
+          resolve: (payload) => payload
+        }
+      }
+    }
   },
   endpoint: '/api/graphql'
 });
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,42 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { useState, useEffect } from "react";
+import { ApolloClient, InMemoryCache, ApolloLink, gql } from "@apollo/client";
+import { YogaLink } from "@graphql-yoga/apollo-link";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new YogaLink({
+    endpoint: "/api/graphql",
+  })
+});
 
 export default function Home() {
   const [greeting, setGreeting] = useState(null);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    fetch('/api/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-          query {
-            hello
-          }
-        `,
-      }),
-    })
-      .then(res => res.json())
-      .then(res => setGreeting(res.data.hello));
+    client
+      .query({
+        query: gql`
+        query {
+          hello
+        }`
+      })
+      .then((res) => setGreeting(res.data.hello))
+      .catch((err) => console.log(err));
   }, [greeting]);
+
+  client.subscribe({
+    query: gql`
+    subscription {
+      notification
+    }`
+  }).subscribe({
+    next: ({ data }) => setNotification(data.notification)
+  });
+
 
   return (
     <div className={styles.container}>
@@ -34,9 +47,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {greeting || 'Loading...'}
-        </h1>
+        <h1 className={styles.title}>{greeting || "Loading..."}</h1>
+        {notification && <p className={styles.description}>{notification}</p>}
       </main>
 
       <footer className={styles.footer}>
@@ -45,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
